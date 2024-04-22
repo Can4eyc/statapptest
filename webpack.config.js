fc388cadf225af9d036ab4cc2e81dd6ba640771c
Aleksandr Nastasienko <nastasiovich@ukr.net>
2024-04-22 22:18:10 +0000

    chnged webpack_conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
+const path = require('path');
+
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: '@sucrase/webpack-loader',
-					options: {
-						transforms: ['jsx']
-					}
-				}
-			}
-		]
-	}
+  entry: './src/client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'), // Output to the "public" directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: '@sucrase/webpack-loader',
+          options: {
+            transforms: ['jsx']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'development', // or 'production' depending on your needs
 };
+
